d364c8d9c2a147e83855816188cd3ef81b94b18b
maurolepore <maurolepore@gmail.com>
2018-06-12 11:09:44 +0000

    Prepare demo

--- R/06-formatting-dates.R
+++ R/06-formatting-dates.R
@@ -1,35 +1,22 @@
 # install.packages("lubridate")
-library("lubridate")
+# TODO: Load ("open") __lubridate__
 
 # Allow Restarting R session (shift + ctrl/cmd + F10)
 surveys <- read.csv("stuff/portal_data_joined.csv")
 
-# Create a date object with `ymd()` and inspect the structure
+# TODO: Create a date object with `ymd()` and inspect the structure
 
-date_chr <- "2015-01-01"
-# Same
-date_chr <- paste("2015", "1", "1", sep = "-")
+# TODO: Compare structure of a string and date object
 
-# They look similar but behave differently. Compare:
-str(date_chr)
-str(ymd(date_chr))
+# TODO: Sum 1 + date_chr
+# TODO: Sum 1 + date_date
 
-# Fails
-date_chr + 1
-
-# Works
-ymd(date_chr) + 1
-
-my_date <- ymd(date_chr) 
-str(my_date)
-
-# Create a `date` variable in `surveys` from its vars `year`, `month` and `day`
-surveys$date <- ymd(paste(surveys$year, surveys$month, surveys$day, sep = "-"))
-head(surveys)
+# TODO: Create a `date` col in `surveys` from its vars `year`, `month` and `day`
+# TODO: See first few lines of `surveys`
 
 # Explore parsing errors due to missing values
-is_missing_date <- is.na(surveys$date)
-missing_dates <- surveys[is_missing_date, c("year", "month", "day")]
-unique(missing_dates)
+# TODO: Create a logical vector of positions of missing values of `date`
+# TODO: Subset `surveys` with the logical vector created above
+# TODO: See unique values of missing dates
 
 # What's the problem? (Hint: check your calendar)
